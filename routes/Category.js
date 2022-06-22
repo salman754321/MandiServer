a88debb9083ac5756735892db89a6a448beb492d
Salman iqbal <salman754321@gmail.com>
2022-06-22 19:10:47 +0500

    Heroku

--- routes/Category.js
+++ routes/Category.js
@@ -2,16 +2,16 @@ var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
 const authenticate = require('../authenticate');
-const upload = require('../utils/uploader');
+const upload = require('../utils/uploader'  );
 router.use(bodyParser.json());
-const {getAllCategory ,addCategory , deleteCategory , getCategoryById}  = require('../controller/CategoryController');
+const {getAllCategory ,addCategory , deleteCategory , getCategoryById , editCategory}  = require('../controller/CategoryController');
 
-// onlu loggedin users can access this fincion getAllCategory
+// onlu loggedin users can access this fincion getAllCategory   
 router.get('/', authenticate.verifyUser , getAllCategory);
 // only admin can access this function addCategory
 router.post('/add', authenticate.verifyUser, authenticate.verifyAdmin ,upload.single('image')  ,addCategory);
 
-
+router.post('/edit/:id' , authenticate.verifyUser , authenticate.verifyAdmin , upload.single('image') , editCategory);
 router.delete('/delete/:id', authenticate.verifyUser, authenticate.verifyAdmin ,deleteCategory);
 
 router.get('/:id', authenticate.verifyUser , getCategoryById);
